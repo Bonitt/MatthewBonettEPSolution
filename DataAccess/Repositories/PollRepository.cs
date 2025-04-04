c012034ccd4dce64796797d499ec8051b5846c01
Bonitt <matthewbonett1309@gmail.com>
2025-04-04 13:51:56 +0200

    Creating Domain, DataAccess and Presentations along side the PollDbContext, Poll and PollRepository files

---
+++ DataAccess/Repositories/PollRepository.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.DataContext;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class PollRepository
+    {
+
+        private PollDbContext _context;
+
+        public PollRepository(PollDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePoll(string title, string option1, string option2, string option3)
+        {
+            var poll = new Poll
+            {
+                Title = title,
+                Option1Text = option1,
+                Option2Text = option2,
+                Option3Text = option3,
+                Option1VotesCount = 0,
+                Option2VotesCount = 0,
+                Option3VotesCount = 0,
+                DateCreated = DateTime.UtcNow
+            };
+
+            _context.Polls.Add(poll);
+            _context.SaveChanges();
+        }
+
+
+    }
+}
